8966d080718f1fe73302140d169d5f4e557b2fbe
piesieomane <piesieyeboah@gmail.com>
2022-07-13 20:33:14 +0300

    remove comments

--- src/add-remove.test.js
+++ src/add-remove.test.js
@@ -1,12 +1,12 @@
 /**
  * @jest-environment jsdom
  */
-import Todos from './todos.js';
-import Task from './task.js';
-import Store from './store.js';
+import Todos from "./todos.js";
+import Task from "./task.js";
+import Store from "./store.js";
 
-describe('Todos', () => {
-  test('Add Todo Functionality', () => {
+describe("test add and delete functions", () => {
+  test("Add Todo Functionality", () => {
     // arrange
     document.body.innerHTML = `
     <form id="todo-form">       
@@ -18,15 +18,15 @@ describe('Todos', () => {
     </ul>
     `;
     // act
-    const task = new Task(1, 'hello');
+    const task = new Task(1, "hello");
     Todos.add(task);
 
     // assert
-    const listItem = document.querySelector('#tasks .task p');
-    expect(listItem.innerText).toBe('hello');
+    const listItem = document.querySelector("#tasks .task p");
+    expect(listItem.innerText).toBe("hello");
   });
 
-  test('Remove Todo Functionality', () => {
+  test("Remove Todo Functionality", () => {
     // arrange
     document.body.innerHTML = `
     <form id="todo-form">       
@@ -41,33 +41,12 @@ describe('Todos', () => {
     </ul>
     `;
     // act
-    const task = new Task(1, 'hello');
+    const task = new Task(1, "hello");
     Todos.add(task);
-    const listItem = document.querySelector('#tasks .task p');
+    const listItem = document.querySelector("#tasks .task p");
     Store.deleteTodo(listItem);
     // assert
 
     expect(listItem.innerText).toBe(undefined);
   });
 });
-
-// test ('Add Todo Functionality', () => {
-//   // arrange
-//   document.body.innerHTML = `
-//   <form id="todo-form">
-//   <input type="text" name="todo" id="todo" placeholder="Add to your list...">
-//   <input type="submit" value="&#x23CE;">
-//   </form>
-//   <ul id="tasks">
-
-//   </ul>
-//   `
-
-//   // act
-//   const task = new Task(1, 'hello');
-//   Todos.add(task);
-
-//   // assert
-//   const listItem = document.querySelector('#tasks .task p')
-//   expect(listItem.innerText).toBe('hello')
-// })
