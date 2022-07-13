0e51655f33c46a2296af87ffa7dc4e2c3e284995
piesieomane <piesieyeboah@gmail.com>
2022-07-13 20:31:35 +0300

    fix linter errors

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 export default class Store {
   // Get Todos from Local Storage
   static getTodos() {
-    const todos = JSON.parse(localStorage.getItem("todos")) || [];
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
     return todos;
   }
 
@@ -9,21 +9,20 @@ export default class Store {
     // Add a Todo to Local Storage
     const tasks = Store.getTodos();
     tasks.push(task);
-    localStorage.setItem("todos", JSON.stringify(tasks));
+    localStorage.setItem('todos', JSON.stringify(tasks));
   }
 
   static setTodos(todos) {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem('todos', JSON.stringify(todos));
   }
 
   // Delete Todo From Local Storage
   static deleteTodo(task) {
     task.parentElement.remove();
-    if (task.classList.contains("fa-trash-can")) {
+    if (task.classList.contains('fa-trash-can')) {
       let todos = Store.getTodos();
-      const des =
-        task.previousSibling.firstChild.innerHTML ||
-        task.previousSibling.innerHTML;
+      const des = task.previousSibling.firstChild.innerHTML
+        || task.previousSibling.innerHTML;
       todos = todos.filter((t) => t.description !== des);
       todos.forEach((todo, i) => {
         todo.index = i + 1;
