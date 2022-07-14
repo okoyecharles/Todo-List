1594b027c9034af2324c90decf5ca2423a4aeee0
piesieomane <piesieyeboah@gmail.com>
2022-07-14 16:09:16 +0300

    resolvee conflict two

--- src/add-remove.test.js
+++ src/add-remove.test.js
@@ -3,24 +3,50 @@
  */
 import Todos from './todos.js';
 import Task from './task.js';
+import Store from './store.js';
 
-test('Add Todo Functionality', () => {
-  // arrange
-  document.body.innerHTML = `
-  <form id="todo-form">       
-  <input type="text" name="todo" id="todo" placeholder="Add to your list...">
-  <input type="submit" value="&#x23CE;">
-  </form>
-  <ul id="tasks">
+describe('test add and delete functions', () => {
+  test('Add Todo Functionality', () => {
+    // arrange
+    document.body.innerHTML = `
+     <form id="todo-form">       
+     <input type="text" name="todo" id="todo" placeholder="Add to your list...">
+     <input type="submit" value="&#x23CE;">
+     </form>
+     <ul id="tasks">
+   
+     </ul>
+     `;
+    // act
+    const task = new Task(1, 'hello');
+    Todos.add(task);
 
-  </ul>
-  `;
+    // assert
+    const listItem = document.querySelector('#tasks .task p');
+    expect(listItem.innerText).toBe('hello');
+  });
 
-  // act
-  const task = new Task(1, 'hello');
-  Todos.add(task);
+  test('Remove Todo Functionality', () => {
+    // arrange
+    document.body.innerHTML = `
+     <form id="todo-form">       
+     <input type="text" name="todo" id="todo" placeholder="Add to your list...">
+     <input type="submit" value="&#x23CE;">
+     </form>
+     <ul id="tasks">
+     <li class="task">
+     <p>hello</p>
+     <button class="remove">&#x2715;</button>
+     </li>
+     </ul>
+     `;
+    // act
+    const task = new Task(1, 'hello');
+    Todos.add(task);
+    const listItem = document.querySelector('#tasks .task p');
+    Store.deleteTodo(listItem);
+    // assert
 
-  // assert
-  const listItem = document.querySelector('#tasks .task p');
-  expect(listItem.innerText).toBe('hello');
+    expect(listItem.innerText).toBe(undefined);
+  });
 });
